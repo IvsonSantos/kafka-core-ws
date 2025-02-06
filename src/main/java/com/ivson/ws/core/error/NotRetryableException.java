333e85e4fcb7cf8df5c6ce0bf6f68e6b15ba4af2
isantos <ivson.santos@ottogroup.com>
2025-02-06 13:47:02 +0100

    Classe a ser compartilhada

---
+++ src/main/java/com/ivson/ws/core/error/NotRetryableException.java
@@ -0,0 +1,13 @@
+package com.ivson.ws.core.error;
+
+public class NotRetryableException extends RuntimeException {
+
+    public NotRetryableException(Exception exception) {
+        super(exception);
+    }
+
+    public NotRetryableException(String message) {
+        super(message);
+    }
+
+}
